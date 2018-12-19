367b4d52c4b6b55ada49c40f999b8a406c912a5f
huangqiankun123 <18671166183@163.com>
2018-12-19 22:10:51 +0800

    首页头布局

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {Button, Row, Col} from "vant";
+
+Vue.use(Button).use(Row).use(Col)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div class="search-bar">
+            <van-row>
+                <van-col span="3">
+                    <img :src="locationIcon" width="80%" class="location-icon"/>
+                </van-col>
+                <van-col span="16">
+                    <input class="search-input" />
+                </van-col>
+                <van-col span="5">
+                    <van-button size="mini">查找</van-button>
+                </van-col>
+            </van-row>
+        </div>
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        data() {
+            return {
+                locationIcon:require('../common/images/icon_location.png')
+            }
+        }
+
+    }
 </script>
+
+<style lang="stylus" scoped>
+    .search-bar
+        height: 2.2rem
+        background-color: #e5017d
+        line-height: 2.2rem
+        .location-icon
+            padding-left :0.3rem
+            padding-top :0.2rem
+        .search-input
+            width :100%
+            height :1.3rem
+            border-top:0
+            border-left :0
+            border-right :0
+            border-bottom :1px solid #fff !important
+            background-color :#e5017d
+            color :white
+</style>
