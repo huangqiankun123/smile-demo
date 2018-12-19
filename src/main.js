84b9608a545ea4bf4272b71394fd95a55c123351
huangqiankun123 <18671166183@163.com>
2018-12-19 22:28:59 +0800

    首页轮播图  图片地址有问题

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import {Button, Row, Col} from "vant";
+import {Button, Row, Col, Swipe, SwipeItem } from "vant";
 
-Vue.use(Button).use(Row).use(Col)
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,21 @@
                     <img :src="locationIcon" width="80%" class="location-icon"/>
                 </van-col>
                 <van-col span="16">
-                    <input class="search-input" />
+                    <input class="search-input"/>
                 </van-col>
                 <van-col span="5">
                     <van-button size="mini">查找</van-button>
                 </van-col>
             </van-row>
         </div>
-
+        <!--swipwer area-->
+        <div class="swipe-area">
+            <van-swipe :autoplay="1000">
+                <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
+                    <img :src="banner.imageUrl" width="100%"/>
+                </van-swipe-item>
+            </van-swipe>
+        </div>
     </div>
 </template>
 
@@ -23,7 +30,12 @@
         name: 'home',
         data() {
             return {
-                locationIcon:require('../common/images/icon_location.png')
+                locationIcon: require('../common/images/icon_location.png'),
+                bannerPicArray: [
+                    {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
+                    {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
+                    {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'},
+                ]
             }
         }
 
@@ -35,16 +47,18 @@
         height: 2.2rem
         background-color: #e5017d
         line-height: 2.2rem
+
         .location-icon
-            padding-left :0.3rem
-            padding-top :0.2rem
+            padding-left: 0.3rem
+            padding-top: 0.2rem
+
         .search-input
-            width :100%
-            height :1.3rem
-            border-top:0
-            border-left :0
-            border-right :0
-            border-bottom :1px solid #fff !important
-            background-color :#e5017d
-            color :white
+            width: 100%
+            height: 1.3rem
+            border-top: 0
+            border-left: 0
+            border-right: 0
+            border-bottom: 1px solid #fff !important
+            background-color: #e5017d
+            color: white
 </style>
